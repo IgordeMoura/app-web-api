60eef62d4959375ea8b1f19a48cf72106995cf4c
IgordeMoura <igormoura14@yahoo.com.br>
2024-04-11 20:08:41 -0300

    Repositorio atualizado devido a falta na ultima aula - igor

--- projeto-react-api-1/src/pages/NovoLivro.js
+++ projeto-react-api-1/src/pages/NovoLivro.js
@@ -1,8 +1,58 @@
+import { useEffect, useState } from 'react';
 import Input from '../components/form/Input';
 
 import styles from './NovoLivro.module.css';
+import Select from '../components/form/Select/Select';
 
 function NovoLivro (){
+
+    /* State de Dados das Categorias Vindas do Arquivo db.json */
+    const [categories, setCategories] = useState([]);
+
+    /* State de Dados que Vai Armazenar Objeto Json de Livro */
+    const [book, setBook] = useState({});
+    
+    /* Recupera os Dados de Categoria do Arquivo db.json */
+    useEffect(() =>{
+        fetch(
+            'http://localhost:5000/categories',
+            {
+                method: 'GET',
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            }
+        ).then(
+            (response) => response.json()
+        ).then(
+            (data) =>{
+                setCategories(data)
+            }
+        ).catch(
+            (error) =>{
+                console.log(error);
+            }
+        )
+    }, []);
+    
+    /* Handler de Captura dos Dados de Input */
+    function handlerChangeBook(event){
+
+        setBook({...book, [event.target.name] : event.target.value});
+        console.log(book)
+    }
+
+    /* Handler de Captura dos Dados de Select*/
+    function handlerChangeCategory(event){
+
+        setBook({...book, category:{
+            id: event.target.value,
+            category: event.target.options[event.target.selectedIndex].text
+        }});
+    }
+
+    console.log(book)
+    
     return(
         <section className={styles.novo_livro_container}>
 
@@ -16,7 +66,8 @@ function NovoLivro (){
                     id='nome_livro'
                     placeholder='digite o titulo do livro'
                     text='digite o titulo do livro'
-                />
+                    handlerOnChange={handlerChangeBook}
+                    />
 
                 <Input
                     type='text'
@@ -24,7 +75,8 @@ function NovoLivro (){
                     id='nome_autor'
                     placeholder='digite o nome do autor'
                     text='digite o nome do autor'
-                />
+                    handlerOnChange={handlerChangeBook}
+                    />
 
                 <Input
                     type='text'
@@ -32,6 +84,7 @@ function NovoLivro (){
                     id='descricao'
                     placeholder='digite uma descrição para o livro'
                     text='descrição'
+                    handlerOnChange={handlerChangeBook}
                 />
 
                 {/* <p>
@@ -45,6 +98,13 @@ function NovoLivro (){
                 <p>
                     <input type='text' placeholder='Descrição do Livro'/>
                 </p> */}
+                
+                <Select
+                    name='categoria_id'
+                    text='Categoria'
+                    options={categories}
+                    handlerOnChange={handlerChangeCategory}
+                />
 
                 <p>
                     <input type='submit' value='Cadastrar livro'/>
