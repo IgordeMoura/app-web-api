a0ed57302011c4596e0fc428a152e285fad128c0
IgordeMoura <igormoura14@yahoo.com.br>
2024-05-16 20:57:50 -0300

    Desenvolvimento Botão Delete Livro - Igor

--- projeto-react-api-1/src/components/CardBook/CardBook.js
+++ projeto-react-api-1/src/components/CardBook/CardBook.js
@@ -1,6 +1,11 @@
 import styles from './CardBook.module.css';
 
-function CardBook({id, livro, autor, category}){
+function CardBook({id, livro, autor, category, handlerRemove}){
+
+    const remove = (event)=>{
+        event.preventDefault();
+        handlerRemove(id);
+    }
 
     return(
         <div className={styles.book_card}>
@@ -12,7 +17,12 @@ function CardBook({id, livro, autor, category}){
             <p className={styles.category_text}>
                 <span></span> {category}
             </p>
-            
+
+            <div className={styles.card_book_actions}>
+                <button onClick={remove}>
+                    Excluir
+                </button>
+            </div>
         </div>
     )
 }
--- projeto-react-api-1/src/pages/Livros.js
+++ projeto-react-api-1/src/pages/Livros.js
@@ -11,6 +11,9 @@ function Livros (){
 
     const [books, setBooks] = useState([]);
 
+    // Estado de dados da Mensagem de Exclusão de Livros
+    const [bookMessage, setBookMessage] = useState('');
+
     useEffect(()=>{
         
         fetch('http://localhost:5000/books', {
@@ -23,7 +26,29 @@ function Livros (){
             .then((data)=>{setBooks(data)})
             .catch((err)=>{console.log(err)});
 
-    }, []);
+    }, [books]);
+
+    // Função de Exclusão de Livro
+    function removeBooks(id){
+
+        fetch(`http://localhost:5000/books/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type':'application/json'
+            },
+        })
+        .then(resp=> resp.json())
+        .then(
+            (data) => {
+                // setBooks(books.filter((book_data)=> book_data.id != id))
+                // alert('Livro Exclusivo!')
+
+                setBookMessage('Livro Excluido Com Sucesso');
+            }
+        )
+        .catch(err=>console.log(err));
+
+    }
 
 
     const location = useLocation();
@@ -40,10 +65,21 @@ function Livros (){
 
             <h1>Aqui Serão Listados os Seus <span>Livros!</span></h1>
             
+            {/* Mensagem de Sucesso Para Cadastro */}
             {
                 message && (
                     <Message
                         msg={message}
+                        type='success'
+                    />                    
+                )
+            }
+
+            {/* Mensagem de Sucesso para Exclusivo */}
+            {
+                bookMessage && (
+                    <Message
+                        msg={bookMessage}
                         type='success'
                     />                    
                 )
@@ -59,6 +95,8 @@ function Livros (){
                         livro={book.nome_livro}
                         autor={book.nome_autor}
                         category={book.category.category}
+                        key={book.id}
+                        handlerRemove={removeBooks}
                     />
                 ])
             }
