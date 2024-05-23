901158ecd908d6588c0ea00b7ca4f4a25d98e764
IgordeMoura <igormoura14@yahoo.com.br>
2024-05-23 20:26:58 -0300

    Desenvolvimento e Finalização Pagina de Edição de Livros - Igor

--- projeto-react-api-1/src/pages/EditarLivros.js
+++ projeto-react-api-1/src/pages/EditarLivros.js
@@ -1,13 +1,15 @@
 import styles from './EditarLivro.module.css';
 
 import { useState, useEffect } from 'react';
-import { useParams  } from 'react-router-dom';
+import { useNavigate, useParams  } from 'react-router-dom';
 
 import Input from '../components/form/Input';
 import Select from '../components/form/Select/Select';
 
 
 function EditarLivros(params){
+    const navigate = useNavigate();
+
     /* State de Dados das Categorias Vindas do Arquivo db.json */
     const [categories, setCategories] = useState([]);
 
@@ -19,6 +21,7 @@ function EditarLivros(params){
 
     /* Recupera os Dados de Categoria do Arquivo db.json */
     useEffect(() =>{
+
         fetch(
             'http://localhost:5000/categories',
             {
@@ -41,8 +44,8 @@ function EditarLivros(params){
     }, []);
 
     // Recuperando os Dados de Livro Para a Edição
-    useEffect(()=>{
-            
+    useEffect(()=>{ 
+
         fetch(`http://localhost:5000/books/${id}`, {
             method: 'GET',
             headers: {
@@ -74,49 +77,74 @@ function EditarLivros(params){
         }});
     }
 
+    // Funcionalidade de Edição de Livro
+    function editBook(book){
+
+        fetch(`http://localhost:5000/books/${book.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(book)
+        })
+        .then (resp => resp.json())
+        .then ((data) => {
+            setBook(data)
+            navigate('/livros', {state: 'Livro Alterado com Sucesso!'})
+        })
+        .catch (err=>(console.log(err)));
+    }
+
+    // FUnção de Submit Controlado dos Dados
+    function submit (event){
+        event.preventDefault();
+        editBook(book);
+    }
+
     return(
         <div className={styles.book_container}>
             <h1>Edição de Livros</h1>
 
-            <form>
+            <form onSubmit={submit}>
 
-            <Input
-                type='text'
-                name='nome_livro'
-                id='nome_livro'
-                placeholder='digite o titulo do livro'
-                text='digite o titulo do livro'
-                value={book.nome_livro}
-                handlerOnChange={handlerChangeBook}
-                />                
+                <Input
+                    type='text'
+                    name='nome_livro'
+                    id='nome_livro'
+                    placeholder='digite o titulo do livro'
+                    text='digite o titulo do livro'
+                    value={book.nome_livro}
+                    handlerOnChange={handlerChangeBook}
+                    />                
 
-            <Input
-                type='text'
-                name='nome_autor'
-                id='nome_autor'
-                placeholder='digite o nome do autor'
-                text='digite o nome do autor'
-                value={book.nome_autor}
-                handlerOnChange={handlerChangeBook}
+                <Input
+                    type='text'
+                    name='nome_autor'
+                    id='nome_autor'
+                    placeholder='digite o nome do autor'
+                    text='digite o nome do autor'
+                    value={book.nome_autor}
+                    handlerOnChange={handlerChangeBook}
+                    />
+
+                <Input
+                    type='text'
+                    name='descricao'
+                    id='descricao'
+                    placeholder='digite uma descrição para o livro'
+                    text='descrição'
+                    value={book.descricao}
+                    handlerOnChange={handlerChangeBook}
+                    />
+                
+                <Select
+                    name='categoria_id'
+                    text='Categoria'
+                    options={categories}
+                    handlerOnChange={handlerChangeCategory}
                 />
 
-            <Input
-                type='text'
-                name='descricao'
-                id='descricao'
-                placeholder='digite uma descrição para o livro'
-                text='descrição'
-                value={book.descricao}
-                handlerOnChange={handlerChangeBook}
-                />
-            
-            <Select
-                name='categoria_id'
-                text='Categoria'
-                options={categories}
-                handlerOnChange={handlerChangeCategory}
-            />
-
+                <button type='submit' value='Editar'>teste</button>
             </form>
         </div>
     );
